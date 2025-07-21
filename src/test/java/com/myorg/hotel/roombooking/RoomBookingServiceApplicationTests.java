0671c002f2227c20d1bba3ec7b619862b3673187
Muthamil Selvam Natarajan <muthamilselvamn@gmail.com>
2025-07-22 01:35:16 +0530

    Initial commit: Spring Boot Room Booking Service setup

---
+++ src/test/java/com/myorg/hotel/roombooking/RoomBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.hotel.roombooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
